881f187ae67ee6c6b6cf25f4c0a4024b71c8920b
gwcms <info@gw.lt>
2012-10-08 13:51:32 +0300

    For debuging, development, create table from associative array

--- admin/lib/helpers/gw_data_to_html_table_helper.class.php
+++ admin/lib/helpers/gw_data_to_html_table_helper.class.php
@@ -18,7 +18,7 @@ class GW_Data_to_Html_Table_Helper
 		{
 			$str.="<tr><td>".$i."</td>";
 			foreach($row as $field => $val)
-				$str.="<td>".htmlspecialchars($val)."</td>";
+				$str.="<td>".htmlspecialchars( is_array($val) ? implode(", \n",$val) : $val) ."</td>";
 			$str.="</tr>\n";
 		}		
 		$str.="</table>";
