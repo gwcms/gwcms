72b664b0fcd2984305b20dde9abda52330b4060b
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2015-02-18 00:36:02 +0200

    numeric validator

---
+++ framework/validators/gw_numeric_validator.class.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+class GW_Numeric_Validator extends GW_Validator 
+{	
+	
+	function init()
+	{
+		$this->setParam('error_message', '/VALIDATION/NUMERIC_FAIL');
+	}
+	
+
+	function isValid()
+	{
+		$value = $this->validation_object;
+		
+		$this->reset();
+		
+		$value = trim($value);
+	
+		if (!$value && !$this->getParam('required'))
+			return true;
+		
+		if($value && !is_numeric($value)){
+			$this->setErrorMessage($this->getParam('error_message'));
+			return false;
+		}
+		
+		return true;
+	}
+	
+	function __countWords($value)
+	{
+		$words = 0;
+		
+		foreach (explode(' ', $value) as $word)
+			if (preg_match('/[\w]/', $word))
+				$words++;
+
+		return $words;
+	}
+}
+
+?>
