a61aad225a4fc35e7a6752a21668526ada20f33e
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-02-11 20:19:59 +0200

    gw_temp_data model, http_agent updates, using privoxy

--- framework/net/gw_http_agent.class.php
+++ framework/net/gw_http_agent.class.php
@@ -16,7 +16,7 @@ class GW_Http_Agent
 
 	public $headers=Array(
 		//'Alive'=>'300',
-		'User-Agent'=>'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+		'User-Agent'=>'Mozilla/5.0',
 		//'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 		//'Accept-Language'=>'en-US,en;q=0.5',
 		//'Accept-Encoding'=>'gzip, deflate',
