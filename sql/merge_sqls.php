c055879ab453eb8c541c1177305bc77cf33bd969
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-02-15 13:51:29 +0200

    tool to merge sqls into one file

---
+++ sql/merge_sqls.php
@@ -0,0 +1,22 @@
+<?php
+
+$files = glob(__DIR__.'/2010-*.sql');
+
+$str = '';
+$str .="#File count: ".count($files)."\n";
+
+foreach($files as $file)
+{
+	$str .= '#------------------------------'.basename($file)."-----------------------------------------------------------------------\n";
+	
+	$str .= "\n".file_get_contents($file)."\n";
+	
+	$str .= "\n";
+}
+
+//file_put_contents(__DIR__."/../repository/.sys/temp/merge_".date('Ymd_his').".sql", $str);
+
+
+header('Content-type: text/plain');
+
+echo $str;
