5f07ebb3ddf82b25e9b0160b71dcba3135b82e39
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-02-24 12:12:33 +0200

    autoformat

--- framework/validators/gw_sms_sender_validator.class.php
+++ framework/validators/gw_sms_sender_validator.class.php
@@ -1,45 +1,36 @@
 <?php
 
+class GW_SMS_Sender_Validator extends GW_Validator {
 
-
-class GW_SMS_Sender_Validator extends GW_Validator 
-{	
-	
-	function init()
-	{
-		$this->setParam('error_invalid', '/G/VALIDATION/INVALID_SMS_SENDER');
-	}
-	
-
-	function isValid()
-	{
-		$value = $this->validation_object;
-		
-		$this->reset();
-		
-		$value = trim($value);
-			
-		if (!$value && !$this->getParam('required'))
-			return true;
-		
-		
-		if (
-			(is_numeric($value) && strlen($value) > 16) || 
-			(!is_numeric($value) && strlen($value) > 11)
-		) {
-			$this->setErrorMessage($this->getParam('error_invalid'));
-			return false;
+		function init() {
+				$this->setParam('error_invalid', '/G/VALIDATION/INVALID_SMS_SENDER');
 		}
 
-		if (!empty($value))
-			return true;
-		
-		$this->setErrorMessage($this->getParam('error_message'));
-		
-		return false;
-	}
-	
+		function isValid() {
+				$value = $this->validation_object;
+
+				$this->reset();
+
+				$value = trim($value);
+
+				if (!$value && !$this->getParam('required'))
+						return true;
+
+
+				if (
+						(is_numeric($value) && strlen($value) > 16) ||
+						(!is_numeric($value) && strlen($value) > 11)
+				) {
+						$this->setErrorMessage($this->getParam('error_invalid'));
+						return false;
+				}
+
+				if (!empty($value))
+						return true;
+
+				$this->setErrorMessage($this->getParam('error_message'));
+
+				return false;
+		}
 
 }
-
-?>
