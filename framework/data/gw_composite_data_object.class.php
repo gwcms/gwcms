315fc2571c8235c95b095b8e4f49275118769254
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-02-29 17:52:30 +0200

    after composite item save

--- framework/data/gw_composite_data_object.class.php
+++ framework/data/gw_composite_data_object.class.php
@@ -20,6 +20,7 @@ class GW_Composite_Data_Object Extends GW_Data_Object {
 	}
 
 	function saveCompositeItems($update_context) {
+		$saved=0;
 		foreach ($this->composite_content_base as $field => $item) {
 			//if it is requested to update only some fields not all
 			if (isset($update_context['update_only']))
@@ -28,7 +29,12 @@ class GW_Composite_Data_Object Extends GW_Data_Object {
 
 			$item->setOwnerObject($this, $field);
 			$item->save();
+			
+			$saved=1;
 		}
+		
+		if($saved)
+			$this->fireEvent('AFTER_COMPOSITE_ITEM_SAVE');
 	}
 
 	function removeCompositeItem($field) {
