e79bc876a2141b13e076780c15afe5942b58f90d
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-06-08 11:02:31 +0300

    test

--- config/environment.php
+++ config/environment.php
@@ -5,6 +5,7 @@ define('GW_ENV_TEST',2);
 define('GW_ENV_PROD',3);
 
 
+GW::s("APP_BACKGROUND_REQ_TYPE", 'force_http'); // can be force_http or localhost_base (past one requires valid SITE_LOCAL_URL)
 
 
 if(__DIR__=='/var/www/prodpath'){
@@ -22,4 +23,5 @@ if(__DIR__=='/var/www/prodpath'){
 	
 	GW::s('PROJECT_ENVIRONMENT', GW_ENV_DEV);
 	//GW::s('DB/UPHD', 'user:pass@host/dbname');
+	
 }
