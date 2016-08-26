2c997e72fe927372a25cceb076367de99f7ca611
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-08-26 10:42:57 +0300

    diff colors on favico on different environments

--- config/environment.php
+++ config/environment.php
@@ -7,13 +7,13 @@ define('GW_ENV_PROD',3);
 
 GW::s("APP_BACKGROUND_REQ_TYPE", 'force_http'); // can be force_http or localhost_base (past one requires valid SITE_LOCAL_URL)
 GW::s('PROJECT_FAVICO_ARGS', 'text=GW&text2=CMS&fs=50&font=EncodeSansNarrow-ExtraLight.ttf');
-GW::s('PROJECT_FAVICO_ARGS', GW::s('PROJECT_FAVICO_ARGS').'&color=ff6600');
+
 
 GW::s('DB/INIT_SQLS',"SET SESSION sql_mode = '';"); // automatycaly turned to strict in mysql 5.7 which causes default errors and others
 
-if(__DIR__=='/var/www/prodpath'){
+if(__DIR__=='/var/www/gwcms/config'){
 	GW::s('PROJECT_ENVIRONMENT', GW_ENV_PROD);
-
+	GW::s('PROJECT_FAVICO_ARGS', GW::s('PROJECT_FAVICO_ARGS').'&color=000099');
 	
 	//GW::s("APP_BACKGROUND_REQ_TYPE", 'localhost_base'); // can be force_http or localhost_base (past one requires valid SITE_LOCAL_URL)
 	//GW::s("SITE_LOCAL_URL",'http://localhost/projectpathunder_localhost/');
@@ -23,6 +23,7 @@ if(__DIR__=='/var/www/prodpath'){
 	GW::s('PROJECT_ENVIRONMENT', GW_ENV_TEST);
 	//GW::s('DB/UPHD', 'user:pass@host/dbname');
 }else{
+	GW::s('PROJECT_FAVICO_ARGS', GW::s('PROJECT_FAVICO_ARGS').'&color=ff6600');
 	
 	GW::s('PROJECT_ENVIRONMENT', GW_ENV_DEV);
 	//GW::s('DB/UPHD', 'user:pass@host/dbname');
