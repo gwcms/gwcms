326474d29eaa36fc796139bc26a79e653c4f5df9
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2016-12-14 15:55:00 +0200

    imdb info movies

--- applications/admin/modules/movies/gw_movie.class.php
+++ applications/admin/modules/movies/gw_movie.class.php
@@ -8,7 +8,7 @@ class GW_Movie extends GW_Composite_Data_Object
 	
 	var $composite_map = Array
 	(
-		'image1' => Array('gw_image', Array('dimensions_resize'=>'800x600', 'dimensions_min'=> '100x100')),
+		'image1' => Array('gw_image', Array('dimensions_resize'=>'200x200', 'dimensions_min'=> '100x100')),
 	);
 	
 	
