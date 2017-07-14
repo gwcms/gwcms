8f0d85f2fcf0c3f452f11e73f6bc0637765623cc
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2017-07-14 13:04:13 +0300

    changes track fix gwcms uptodate

--- config/environment.php
+++ config/environment.php
@@ -18,8 +18,9 @@ if(__DIR__=='/var/www/gwcms/config'){
 	GW::s('PROJECT_ENVIRONMENT', GW_ENV_PROD);
 	GW::s('PROJECT_FAVICO_ARGS', GW::s('PROJECT_FAVICO_ARGS').'&color=000099');
 	
-	//$GLOBALS['version'] = is_file(__DIR__.'/version') ? file_get_contents(__DIR__.'/version') : -1;
-
+	$GLOBALS['version'] = is_file(__DIR__.'/version') ? file_get_contents(__DIR__.'/version') : -1;
+	
+	include $dir['ROOT'].'config/db.php';
 	//GW::s("APP_BACKGROUND_REQ_TYPE", 'localhost_base'); // can be force_http or localhost_base (past one requires valid SITE_LOCAL_URL)
 	//GW::s("SITE_LOCAL_URL",'http://localhost/projectpathunder_localhost/');
 	//GW::s("SITE_URL",'https://project.com/');
---
+++ pullupdates.php
@@ -0,0 +1,32 @@
+<?php
+
+//add to /etc/sudoers line:
+//www-data ALL = (root) NOPASSWD: /usr/bin/php /var/www/karolisvil/padangu-arsenalas.lt/public/pullupdates.php
+
+if(isset($argv)){
+    //parse shell script arguments into $_GET
+    //php -f somefile.php a=1 b[]=2 b[]=3
+    //parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
+
+
+if(isset($_SERVER['REMOTE_ADDR'])){
+	header('Content-type: text/plain');
+		
+	echo "exec res:\n";
+	$res=shell_exec($cmd="sudo /usr/bin/php ".__FILE__.' 2>&1');
+	//echo $cmd;
+	echo $res;
+	echo ".";
+	exit;
+}
+
+
+if(!isset($_SERVER['HTTP_HOST'])){
+	echo "usr {$_SERVER['USER']}. pulling\n";
+	$dir = __DIR__;
+	echo shell_exec("cd '$dir' && git pull 2>&1");
+	echo shell_exec("cd '$dir' && rm repository/.sys/templates_c/*");
+	echo shell_exec("cd '$dir' && git rev-parse --short HEAD > version");	
+	
+}
