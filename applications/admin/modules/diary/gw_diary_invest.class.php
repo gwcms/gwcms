8988059f82c525f14b4557f581a46d365a91c0d8
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2017-09-28 00:21:18 +0300

    investors diary

--- applications/admin/modules/diary/gw_diary_invest.class.php
+++ applications/admin/modules/diary/gw_diary_invest.class.php
@@ -7,7 +7,7 @@ class GW_Diary_Invest extends GW_Composite_Data_Object
 	var $table = 'gw_diary_invest';
 	
 	//var $calculate_fields = Array('child_count'=>1, 'path'=>'getPath', 'title'=>1);
-	var $default_order = 'insert_time ASC';		
+	var $default_order = 'insert_time DESC';		
 	
 	
 }
