4d73baecc75985cca4c4b31b578fd101093e51bd
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2018-04-17 21:31:37 +0300

    project_specific files update

--- config/project_specific_files.php
+++ config/project_specific_files.php
@@ -3,8 +3,13 @@
 
 $paths = "
 config/project.php
+config/project_specific_files.php
+applications/site/config/main.php
 test/*
 applications/admin/modules/customers/*
+applications/admin/modules/diary/*
+applications/admin/modules/movies/*
+pullupdates.php
 ";
 
 $paths=explode("\n", trim($paths));
