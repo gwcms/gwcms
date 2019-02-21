1753938d45e1e01d24152bea8649478bc1e728ce
wdm <wdm@wdm>
2019-02-21 20:26:22 +0200

    skip ad

--- applications/admin/modules/documentation/module_features.class.php
+++ applications/admin/modules/documentation/module_features.class.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class Module_Items extends GW_Common_Module_Tree_Data
+class Module_Features extends GW_Common_Module_Tree_Data
 {	
 
 	
@@ -55,7 +55,88 @@ class Module_Items extends GW_Common_Module_Tree_Data
 		];
 	}
 	
-
 	
+	function doGetTree()
+	{
+		//$list = $this->model->getFullTree();
+		
+		$list0 = $this->model->findAll(false, ['select'=>'id, title, active, parent_id, type, priority', 'key_field'=>'id', 'order'=>'priority']);
+		
+		foreach($list0 as $item){
+			$vals=[
+			    "text"=>$item->title,
+			    "parent"=>$item->parent_id==-1 ? '#' : $item->parent_id,
+			    "id"=>$item->id,
+			    //"state" => ["opened" => true, "selected" => true],
+			    "type" => 't'.$item->type,
+			    "position" => $item->priority
+			];
+			$list[] = $vals;
+		}
+		
+
+		header('content-type: text/plain');
+		echo json_encode($list, JSON_PRETTY_PRINT);
+		exit;
+	}
+	
+	function doRename()
+	{
+		if (!$item = $this->getDataObjectById())
+			return false;
+
+		
+		$item->title = $_GET['title'];
+
+		if($item->isChanged()){
+			$item->updateChanged();
+			$this->setMessage(["text"=>GW::l("/g/SAVE_SUCCESS"), "type"=>GW_MSG_SUCC, "title"=>$item->title, "obj_id"=>$item->id,'float'=>1]);
+		}else{
+			$this->setMessage(["text"=>GW::l("/g/NO_CHANGES"), "type"=>GW_MSG_INFO, "title"=>$item->title, "obj_id"=>$item->id,'float'=>1]);
+		}
+
+		
+
+		//if($this->isPacketRequest())	
+		//	$this->app->addPacket(['action'=>'delete_row','id'=>$item->id, 'context'=>get_class($this->model)]);
+		
+		if(!$this->sys_call)
+			$this->jump();
+	}
+	
+	function doMoveNode()
+	{
+		if (!$item = $this->getDataObjectById())
+			return false;
+
+		
+		$item->parent_id = $_GET['parent']=='#' ? -1 : $_GET['parent'];
+		$item->priority = $_GET['priority'];
+			
+		if($item->isChanged()){
+			
+
+			
+			$item->updateChanged();
+			
+			if(isset($item->changed_fields['priority'])){
+				//$item->fixOrder();
+				//d::dumpas($item->getDB()->last_query);
+			}			
+			
+			
+			$this->setMessage(["text"=>GW::l("/g/SAVE_SUCCESS"), "type"=>GW_MSG_SUCC, "title"=>$item->title, "obj_id"=>$item->id,'float'=>1]);
+		}else{
+			$this->setMessage(["text"=>GW::l("/g/NO_CHANGES"), "type"=>GW_MSG_INFO, "title"=>$item->title, "obj_id"=>$item->id,'float'=>1]);
+		}
+
+		
+
+		//if($this->isPacketRequest())	
+		//	$this->app->addPacket(['action'=>'delete_row','id'=>$item->id, 'context'=>get_class($this->model)]);
+		
+		if(!$this->sys_call)
+			$this->jump();
+	}	
 
 }
