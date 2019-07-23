cf871394864ccfd43d3b7e739a59b6873d3a1e48
wdm <wdm@wdm>
2019-07-24 02:11:48 +0300

    itunes link

--- applications/admin/modules/movies/module_movies.class.php
+++ applications/admin/modules/movies/module_movies.class.php
@@ -178,11 +178,12 @@ class Module_Movies extends GW_Common_Module
 	
 	function __eventAfterSave($item)
 	{
+		/*
 		if(!$item->imdb){
 			//Navigator::backgroundRequest("lt/movies?act=do:BackgroundAterInsert&id=".$item->id);
 			//$this->setMessage("Imdb update background process started");
 			$this->doBackgroundAterInsert($item);
-		}
+		}*/
 		
 		$this->__extendMovieDatabase($item);
 	}
