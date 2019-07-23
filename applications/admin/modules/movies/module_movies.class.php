d11087a6b597ec67011b1a465e271aa76ad2fd7f
wdm <wdm@wdm>
2019-07-24 02:30:26 +0300

    errors add more info

--- applications/admin/modules/movies/module_movies.class.php
+++ applications/admin/modules/movies/module_movies.class.php
@@ -248,7 +248,9 @@ class Module_Movies extends GW_Common_Module
 	
 	function doSearchMovies()
 	{
-		
+		if(!isset($_GET['q']))
+			exit;
+			
 		$query_str = $_GET['q'];
 		$page = $_GET['page'] ?? 1;
 		$query_str = str_replace(".", " ", $query_str);
