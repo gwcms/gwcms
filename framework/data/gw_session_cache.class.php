f52840d13772d1e081bef49d93274d3b4ee80ce5
wdm <wdm@wdm>
2020-07-01 00:05:47 +0300

    fix

--- framework/data/gw_session_cache.class.php
+++ framework/data/gw_session_cache.class.php
@@ -5,9 +5,9 @@ class GW_Session_Cache
 
 	static function get($key)
 	{
-		$_SESSION['GW_SESSION_CACHE'] = $_SESSION['GW_SESSION_CACHE'] ?? [];
+		$_SESSION['GW_SESSION_CACHE'][$key] = $_SESSION['GW_SESSION_CACHE'][$key] ?? null;
 		//php 7.4
-		//$_SESSION['GW_SESSION_CACHE'] ??=[];
+		//$_SESSION['GW_SESSION_CACHE'][$key] ??=[];
 		
 		$var = & $_SESSION['GW_SESSION_CACHE'][$key];
 
@@ -17,9 +17,9 @@ class GW_Session_Cache
 
 	static function set($key, $value, $expires = '10 seconds')
 	{
-		$_SESSION['GW_SESSION_CACHE'] = $_SESSION['GW_SESSION_CACHE'] ?? [];
+		$_SESSION['GW_SESSION_CACHE'][$key] = $_SESSION['GW_SESSION_CACHE'][$key] ?? null;
 		//php 7.4
-		//$_SESSION['GW_SESSION_CACHE'] ??=[];
+		//$_SESSION['GW_SESSION_CACHE'][$key] ??=[];
 		
 		$var = & $_SESSION['GW_SESSION_CACHE'][$key];
 
