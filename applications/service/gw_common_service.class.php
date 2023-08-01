d17e4487008e5cce61dbf14939f01da4f1771576
wdm <wdm@wdm>
2023-08-01 18:56:37 +0300

    gwcms updates php8.2

--- applications/service/gw_common_service.class.php
+++ applications/service/gw_common_service.class.php
@@ -53,6 +53,16 @@ class GW_Common_Service
 	{
 		if (!isset($_SESSION))
 			session_start();
+		
+		
+		
+		//fastcgi -workaround, add line into apache config
+		//SetEnvIfNoCase ^Authorization$ "(.+)" HTTP_AUTHORIZATION=$1
+		
+		if(isset($_SERVER['HTTP_AUTHORIZATION']) && !isset($_SERVER['PHP_AUTH_USER']))
+			list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+		
+
 
 		if (isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_PW'] && $this->checkBasicUser($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
 			$_SESSION['service_user'] = $_SERVER['PHP_AUTH_USER'];
