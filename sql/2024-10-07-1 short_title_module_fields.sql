32408b9a06a379837e612ce6aca832fb553400a1
wdm <wdm@wdm>
2024-10-07 18:39:32 +0300

    gwcms updates

--- sql/2024-10-07-1 short_title_module_fields.sql
+++ sql/2024-10-07-1 short_title_module_fields.sql
@@ -1,2 +1,3 @@
-ALTER TABLE `gw_adm_page_fields` ADD `short_title_lt` VARCHAR(30) NOT NULL AFTER `title_ru`;
-ALTER TABLE `gw_adm_page_fields` ADD `short_title_en` VARCHAR(50) NOT NULL AFTER `short_title_lt`, ADD `short_title_ru` VARCHAR(50) NOT NULL AFTER `short_title_en`;
+ALTER TABLE `gw_adm_page_fields` ADD `short_title_lt` VARCHAR(100) NOT NULL AFTER `title_ru`;
+ALTER TABLE `gw_adm_page_fields` ADD `short_title_en` VARCHAR(100) NOT NULL AFTER `short_title_lt`, ADD `short_title_ru` VARCHAR(100) NOT NULL AFTER `short_title_en`;
+
