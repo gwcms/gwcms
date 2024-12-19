1b3bbe182888bfec74abf66bab4c088147709e09
wdm <wdm@wdm>
2024-12-19 18:39:10 +0200

    shor field make longer maybe for html

---
+++ sql/2024-12-19-1 gw_adm_page_fields.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `gw_adm_page_fields` CHANGE `short_title_lt` `short_title_lt` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL;
+
+ALTER TABLE `gw_adm_page_fields` CHANGE `short_title_en` `short_title_en` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL;
+ALTER TABLE `gw_adm_page_fields` CHANGE `short_title_ru` `short_title_ru` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL;
