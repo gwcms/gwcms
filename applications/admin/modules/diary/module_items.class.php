acb7ed51094d88d4a6fc4b5aea77b17189bba608
wdm <wdm@wdm>
2025-03-06 15:02:07 +0200

    stages config

--- applications/admin/modules/diary/module_items.class.php
+++ applications/admin/modules/diary/module_items.class.php
@@ -94,7 +94,7 @@ class Module_Items extends GW_Common_Module_Tree_Data
 		
 		$crpytkey = $this->__getSecret();
 				
-		$q = GW_DB::prepare_query(["UPDATE my_diary_entries SET text_crpt = AES_ENCRYPT(text, ?) WHERE text_crpt IS NULL", $crpytkey]);
+		$q = GW_DB::prepare_query(["UPDATE diary_entries SET text_crpt = AES_ENCRYPT(text, ?) WHERE text_crpt IS NULL", $crpytkey]);
 		GW::db()->query($q);
 		
 		
