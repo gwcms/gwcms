fdc274b14bbbb8971ed597d7ece862d99913f352
wdm <wdm@wdm>
2025-03-06 14:55:32 +0200

    stages config

--- applications/admin/modules/diary/module_items.class.php
+++ applications/admin/modules/diary/module_items.class.php
@@ -14,8 +14,9 @@ class Module_Items extends GW_Common_Module_Tree_Data
 		
 		
 		
-		d::dumpas($this->module_path);
+		
 		$this->modcfg = $this->initModCfg();
+		d::dumpas($this->modcfg);
 		
 		$this->modcfg->last_request = date('Y-m-d H:i:s');
 		
