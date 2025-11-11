7fcf178ab67c96eac948408725da5d45ec49820e
wdm <wdm@wdm>
2025-11-11 17:07:56 +0200

    speed tweak

--- test/fakegeoip.php
+++ test/fakegeoip.php
@@ -1,6 +1,10 @@
 <?php
 
-function geoip_country_code_by_name()
-{
-	return 'lt';
+if(!function_exists('geoip_country_code_by_name')){
+	function geoip_country_code_by_name($ip)
+	{
+		return file_get_contents('http://127.0.0.1:8000/geoip.php?ip='.$ip);
+
+		//return shell_exec('/usr/bin/php7.4 -r "echo geoip_country_code_by_name(\''.$ip.'\');"');
+	}
 }
